930b47f00f34582e05246b00591c8cc401693af4
Aggrey Lelei <aggreykiplimo@gmail.com>
2020-12-10 11:18:52 +0300

    Add files via upload

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,6 @@
+
 // Called when the user clicks on the browser action.
+//Called
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
   chrome.tabs.executeScript(null, {file: "emoji-translate.js"});
